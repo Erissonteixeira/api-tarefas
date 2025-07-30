d9f964d5d7e91b583ddbc96692b18af41bc16361
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-07-30 19:37:45 -0300

    refactor: controller: padroniza respostas com ResponseEntity no TodoController - métodos create, update e delete agora retornam ResponseEntity para controle de status HTTP - mantém list retornando List<Todo> diretamente - melhora consistência e boas práticas REST

--- src/main/java/io/github/teste/api_crudtarefasx/controller/TodoController.java
+++ src/main/java/io/github/teste/api_crudtarefasx/controller/TodoController.java
@@ -1,34 +1,44 @@
-package io.github.teste.api_crudtarefasx.controller;
+        package io.github.teste.api_crudtarefasx.controller;
 
-import io.github.teste.api_crudtarefasx.model.Todo;
-import io.github.teste.api_crudtarefasx.service.TodoService;
-import org.springframework.web.bind.annotation.*;
+    import io.github.teste.api_crudtarefasx.model.Todo;
+    import io.github.teste.api_crudtarefasx.service.TodoService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+    import java.util.List;
 
-@RestController
-@RequestMapping("/todos")
-public class TodoController {
-    private TodoService todoService;
+    @RestController
+    @RequestMapping("/todos")
+    public class TodoController {
+        private TodoService todoService;
 
-    public TodoController(TodoService todoService) {
-        this.todoService = todoService;
-    }
-    @PostMapping
-    List<Todo> create(@RequestBody Todo todo){
-        return todoService.create(todo);
-    }
-    @GetMapping
-    List<Todo> list(){
-        return todoService.list();
-    }
-    @PutMapping
-    List<Todo> update(@RequestBody Todo todo){
-        return todoService.update(todo);
-    }
-    @DeleteMapping("{id}")
-    List<Todo> delete(@PathVariable("id") Long id){
-        return todoService.delete(id);
+        public TodoController(TodoService todoService) {
+          this.todoService = todoService;
+        }
+        @PostMapping
+        public ResponseEntity<Todo> create(@RequestBody Todo todo){
+            Todo created = todoService.create(todo);
+            return ResponseEntity.ok(created);
+        }
+        @GetMapping
+        public List<Todo> list(){
+            return todoService.list();
+        }
+        @PutMapping
+        public ResponseEntity<Todo> update(@RequestBody Todo todo){
+            Todo updated = todoService.update(todo);
+            return ResponseEntity.ok(updated);
 
+        }
+        @DeleteMapping("{id}")
+        public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+            boolean deleted = todoService.delete(id);
+            if (deleted){
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+
+        }
     }
-}
