f09a7c73a204fa28d0c3b181efa819b01f0bd03e
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-07-30 13:24:46 -0300

    feat: adicionar getters e setters na entidade Todo

--- src/main/java/io/github/teste/api_crudtarefasx/model/Todo.java
+++ src/main/java/io/github/teste/api_crudtarefasx/model/Todo.java
@@ -12,4 +12,45 @@ public class Todo {
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
 }
+
