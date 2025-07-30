26b1a6699be326b2436483416346be1724dd11fc
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-07-30 12:58:53 -0300

    first commit

---
+++ src/test/java/io/github/teste/api_crudtarefasx/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.teste.api_crudtarefasx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
