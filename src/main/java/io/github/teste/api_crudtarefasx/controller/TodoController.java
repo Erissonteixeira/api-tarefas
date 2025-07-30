40da1204ce104c594bf5493f9e720da863fcdb25
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-07-30 16:17:54 -0300

    feat: criar TodoController com endpoints REST para CRUD

---
+++ src/main/java/io/github/teste/api_crudtarefasx/controller/TodoController.java
@@ -0,0 +1,34 @@
+package io.github.teste.api_crudtarefasx.controller;
+
+import io.github.teste.api_crudtarefasx.model.Todo;
+import io.github.teste.api_crudtarefasx.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+
+    }
+}
